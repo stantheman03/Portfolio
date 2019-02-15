a94ba4a1399da92d494ceef29b204294907d9098
Stanley Philip <Stanley Philip>
2019-02-15 14:02:36 -0500

    first

---
+++ portfolio/src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/stantheman03/stantheman03.github.io",
+    "name": "Stanley Philip",
+    "role": "Frontend Developer and Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/stanley-philip-4b7b5672/",
+    "skypeid": "live:sphilip60_1",
+    "roleDescription": "I like developing in various parts of frontend development and like to learn about new technologies, read technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/stanley-philip-4b7b5672/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/stantheman03",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/stantheman03/stantheman03.github.io",
+    "education":[
+      {
+        "UniversityName":"Holy Family University",
+        "specialization":"Radilogical Sciences",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":"Presidential Scholarship Award Recipient"
+      },
+      {
+        "UniversityName":"Saint Jospeh University",
+        "specialization":"Business Intelligence",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":"Dean's List student Fall 2017 && Spring 2018"
+      },
+      {
+        "UniversityName":"University Of Penn",
+        "specialization":"Coding Bootcamp Full Stack Development",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"CSGI",
+        "specialization":"Front End Developer",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Javascript Engineer Intern"
+      }
+      
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5,CSS, jquery,javascript,handlebars, ejs"
+      },
+      {
+        "skillname":"nodejs, expressjs, Mongodb, MySql, Firebase, Mongoose"
+      },
+      {
+        "skillname":"reactjs, reduxjs"
+      },
+      {
+        "skillname":"AJAX, API-Devlopment,MVC, ORM, passport-oAuth,MERN stack"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/girl.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/into-the-light.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/farmerboy.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/retrocam.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
